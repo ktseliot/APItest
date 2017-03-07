0de876751637bf8e88c069121fcebbf562533dde
Katie O'Flynn <koflynn@spartaglobal.co>
2017-03-07 14:25:18 +0000

    First commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "http://www.rubygems.org"
+
+gem 'json'
+gem 'faraday'
+gem 'faraday_middleware'
+gem 'httparty'
+gem 'rest-client'
---
+++ spec/todo_spec.rb
@@ -0,0 +1,52 @@
+require 'net/http'
+require 'faraday'
+require 'faraday_middleware'
+require 'json'
+require 'httparty'
+
+# Net::HTTP.get(URI.parse('http://lacedeamon.spartaglobal.com/todos'))
+
+url = "http://lacedeamon.spartaglobal.com/todos"
+
+describe "To-Dos API" do
+
+  it "should give us a 201 message when we send a post request" do
+    res = HTTParty.post("http://lacedeamon.spartaglobal.com/todos",
+    query: { "title" => "Karan and Katie", "due" => "2017-07-01" } )
+    expect(res.code).to eq(201)
+  end
+
+  it "should give us a json representation of our post request" do
+    # response = HTTParty.get(url)
+    # response.parsed_response
+  end
+
+  it "should not allow us to make a post without a due date" do
+    res = HTTParty.post("http://lacedeamon.spartaglobal.com/todos",
+    query: { "title" => "Karan and Katie"} )
+    expect(res.code).to eq(422)
+  end
+
+  it "should only accept valid characters" do
+
+  end
+
+  it "should allow us to delete a post" do
+    res = HTTParty.delete("http://lacedeamon.spartaglobal.com/todos",
+    query: { "id" => 12568, "title" => "Karan and Katie", "due" => "2017-07-01" } )
+    expect(res.code).to eq(201)
+  end
+
+  it "should not accept dates outside a certain range" do
+    es = HTTParty.post("http://lacedeamon.spartaglobal.com/todos",
+    query: { "title" => "Karan and Katie", "due" => "8017-07-01" } )
+    expect(res.code).to eq(422)
+  end
+
+  it "should not accept past dates" do
+    res = HTTParty.post("http://lacedeamon.spartaglobal.com/todos",
+    query: { "title" => "Karan and Katie", "due" => "2015-07-01" } )
+    expect(res.code).to eq(422)
+  end
+
+end
